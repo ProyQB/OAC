225289835aba982ce46cd7a1dbcf8a9031867ca5
ProyQB <tybinballin2019@gmail.com>
2026-03-16 17:52:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const SUPABASE_URL = "https://qrugfdvdhaxvjqtruzzq.supabase.co";
 const SUPABASE_KEY = "sb_publishable_ZV5TQ1ywOUmB2hPM5DZtnQ_Sgt77oq6";
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
+ 
 let cart = [];
 let currentUser = null;
 
